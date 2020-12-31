3a422492ec12a33c3ebda979df4b5d4d45183e8b
luxinjuan <1769099582@qq.com>
2020-12-31 15:09:25 +0800

    web模块和Express框架

---
+++ 12Express框架/express_demo.js
@@ -0,0 +1,16 @@
+//express_demo.js 文件
+var express = require('express');
+var app = express();
+//Express 应用使用回调函数的参数：request 和 response 对象来处理请求和响应的数据。
+app.get('/', function(req, res) {
+    res.send('Hello World');
+})
+
+var server = app.listen(8081, function() {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
