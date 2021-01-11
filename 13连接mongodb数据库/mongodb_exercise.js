80b3aae6e67694c7c73a4ef0d9a83ee1a7b2354d
luxinjuan <1769099582@qq.com>
2021-01-11 19:31:17 +0800

    分离mongodb

--- 13连接mongodb数据库/mongodb_exercise.js
+++ 13连接mongodb数据库/mongodb_exercise.js
@@ -1,48 +1,69 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
-const dbUrl = 'mongodb://127.0.0.1:27017';
+const { getTestDB, getTestDBSync, getStudents } = require('./mongo_database');
 
-MongoClient.connect(dbUrl, function(err, client) {
-    if (err) {
-        console.log(err);
-        return;
+// getTestDB(function(db){
+//     db.collection('students').dind({age:{$age:20}}).toArray(function(err,resule){
+//         if(err){
+//             console.log(err);
+//             return;
+//         }
+//         console.log(result)
+//     })
+// })
+
+//使用async和await将异步操作编程同步模式
+async function main() {
+    try {
+        const db = await getTestDBSync();
+        const student_collection = db.collection('students');
+        const students = await getStudents(student_collection, { age: { $gte: 20 } });
+        console.log(students)
+    } catch (err) {
+        console.log(err)
     }
-    console.log('连接成功');
-    const testDb = client.db('students');
-    //插入数据
-    // testDb.collection('students').insert({ name: 'li3', age: 22 }, function(err, result) {
-    //     if (err) {
-    //         console.log(err)
-    //         return;
-    //     }
-    //     console.log(result)
-    // })
+}
+main();
 
-    //查找数据
-    // testDb.collection('students').find({ name: 'li3' }).toArray(function(err, result) {
-    //     if (err) {
-    //         console.log(err)
-    //         return
-    //     }
-    //     console.log(result)
-    // })
+// MongoClient.connect(dbUrl, function(err, client) {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log('连接成功');
+//     const testDb = client.db('students');
+//插入数据
+// testDb.collection('students').insert({ name: 'li3', age: 22 }, function(err, result) {
+//     if (err) {
+//         console.log(err)
+//         return;
+//     }
+//     console.log(result)
+// })
 
-    //更新数据
-    // testDb.collection('students').update({ name: 'li3' }, { $set: { name: '李3' } }), (function(err, result) {
-    //     if (err) {
-    //         console.log(err)
-    //         return
-    //     }
-    //     console.log(result)
-    // })
+//查找数据
+// testDb.collection('students').find({ name: 'li3' }).toArray(function(err, result) {
+//     if (err) {
+//         console.log(err)
+//         return
+//     }
+//     console.log(result)
+// })
 
-    //删除数据
-    testDb.collection('students').remove({ name: '李3' }, function(err, result) {
-        if (err) {
-            console.log(err)
-            return
-        }
-        console.log(result)
-    })
-    client.close();
-})
+//更新数据
+// testDb.collection('students').update({ name: 'li3' }, { $set: { name: '李3' } }), (function(err, result) {
+//     if (err) {
+//         console.log(err)
+//         return
+//     }
+//     console.log(result)
+// })
+
+//删除数据
+//     testDb.collection('students').remove({ name: '李3' }, function(err, result) {
+//         if (err) {
+//             console.log(err)
+//             return
+//         }
+//         console.log(result)
+//     })
+//     client.close();
+// })
